1497d2e1a7bd5d332fbbfb288ec6e6c40da7e407
rsqu.rose <rsqu.rose@wind.com.cn>
2026-05-11 15:43:21 +0800

    2026.05.11代码练习

--- src/main/java/com/rsqu/leetcode/hot100/p0046_permute/Hot1000046Permute.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0046_permute/Hot1000046Permute.java
@@ -1,11 +1,35 @@
 package com.rsqu.leetcode.hot100.p0046_permute;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Hot1000046Permute {
 
     public List<List<Integer>> permute(int[] nums) {
-        return null;
+        ArrayList<Integer> cur = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Set<Integer> curNum = new HashSet<>();
+        dfs(nums,curNum,cur,res);
+        return res;
+    }
+
+    private void dfs(int[] nums, Set<Integer> curNum,List<Integer> cur,List<List<Integer>> res) {
+        if (curNum.size()==nums.length) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int num : nums) {
+            if (curNum.contains(num)) {
+                continue;
+            }
+            cur.add(num);
+            curNum.add(num);
+            dfs(nums,curNum,cur,res);
+            curNum.remove(num);
+            cur.remove(cur.size()-1);
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/rsqu/leetcode/hot100/p0049_group_anagrams/Hot1000049GroupAnagrams.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0049_group_anagrams/Hot1000049GroupAnagrams.java
@@ -1,11 +1,25 @@
 package com.rsqu.leetcode.hot100.p0049_group_anagrams;
 
-import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
 
 public class Hot1000049GroupAnagrams {
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        return null;
+        if (strs.length == 0) {
+            return new ArrayList<>();
+        }
+        HashMap<String,List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int[] nums = new int[26];
+            for (char c : str.toCharArray()) {
+                nums[c-'a'] += 1;
+            }
+            String key = Arrays.toString(nums);
+            map.computeIfAbsent(key,value -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
     }
 
     public static void main(String[] args) {
