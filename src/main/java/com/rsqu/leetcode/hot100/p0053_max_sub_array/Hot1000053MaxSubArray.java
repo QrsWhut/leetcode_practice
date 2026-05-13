d7fdf2e7e12043b4986b070a5093094e4e51c0cd
rsqu.rose <rsqu.rose@wind.com.cn>
2026-05-13 11:34:56 +0800

    2026.05.13代码练习

--- src/main/java/com/rsqu/leetcode/hot100/p0053_max_sub_array/Hot1000053MaxSubArray.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0053_max_sub_array/Hot1000053MaxSubArray.java
@@ -3,7 +3,13 @@ package com.rsqu.leetcode.hot100.p0053_max_sub_array;
 public class Hot1000053MaxSubArray {
 
     public int maxSubArray(int[] nums) {
-        return 0;
+        int curMax = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            curMax = Math.max(curMax + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, curMax);
+        }
+        return maxSum;
     }
 
     public static void main(String[] args) {
