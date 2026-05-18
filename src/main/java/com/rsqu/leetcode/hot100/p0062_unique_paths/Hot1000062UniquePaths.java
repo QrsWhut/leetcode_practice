8cd59c4bac28bad3b9f842162662a153258c4c80
rsqu.rose <rsqu.rose@wind.com.cn>
2026-05-18 14:49:09 +0800

    2026.05.18代码练习

--- src/main/java/com/rsqu/leetcode/hot100/p0062_unique_paths/Hot1000062UniquePaths.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0062_unique_paths/Hot1000062UniquePaths.java
@@ -2,8 +2,37 @@ package com.rsqu.leetcode.hot100.p0062_unique_paths;
 
 public class Hot1000062UniquePaths {
 
+    private int[][] dp;
     public int uniquePaths(int m, int n) {
-        return 0;
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        dp = new int[m+1][n+1];
+        int res = dp(m, n);
+        return res;
+    }
+
+    private int dp(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        int up = 0, left = 0;
+        if (dp[m-1][n] != 0) {
+            up = dp[m-1][n];
+        } else {
+            up = dp(m-1, n);
+            dp[m-1][n] = up;
+        }
+        if (dp[m][n-1] != 0) {
+            left = dp[m][n-1];
+        } else {
+            left = dp(m, n-1);
+            dp[m][n-1] = left;
+        }
+        return up + left;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/rsqu/leetcode/hot100/p0064_min_path_sum/Hot1000064MinPathSum.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0064_min_path_sum/Hot1000064MinPathSum.java
@@ -1,9 +1,37 @@
 package com.rsqu.leetcode.hot100.p0064_min_path_sum;
 
+import java.util.Arrays;
+
 public class Hot1000064MinPathSum {
 
+    private static final int UNVISITED = -1;
+
+    private int[][] memo;
+
     public int minPathSum(int[][] grid) {
-        return 0;
+        memo = new int[grid.length][grid[0].length];
+        for (int[] row : memo) {
+            Arrays.fill(row, UNVISITED);
+        }
+
+        return dp(grid.length - 1, grid[0].length - 1, grid);
+    }
+
+    private int dp(int row, int column, int[][] grid) {
+        if (row == 0 && column == 0) {
+            return grid[0][0];
+        }
+        if (row < 0 || column < 0) {
+            return Integer.MAX_VALUE;
+        }
+        if (memo[row][column] != UNVISITED) {
+            return memo[row][column];
+        }
+
+        int up = dp(row - 1, column, grid);
+        int left = dp(row, column - 1, grid);
+        memo[row][column] = Math.min(up, left) + grid[row][column];
+        return memo[row][column];
     }
 
     public static void main(String[] args) {
