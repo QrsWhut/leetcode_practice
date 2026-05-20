f53ebb230b5ce06ed1d7ae141cce59c4f6de4290
rsqu.rose <rsqu.rose@wind.com.cn>
2026-05-20 11:33:25 +0800

    2026.05.20代码练习

--- src/main/java/com/rsqu/leetcode/hot100/p0074_search_matrix_i/Hot1000074SearchMatrixI.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0074_search_matrix_i/Hot1000074SearchMatrixI.java
@@ -3,6 +3,20 @@ package com.rsqu.leetcode.hot100.p0074_search_matrix_i;
 public class Hot1000074SearchMatrixI {
 
     public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int midValue = matrix[mid / n][mid % n];
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
         return false;
     }
 
