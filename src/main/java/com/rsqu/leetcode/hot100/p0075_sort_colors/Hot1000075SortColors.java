6dec82a4bf5e4555058a38f456c3f9ef7e011bfc
rsqu.rose <rsqu.rose@wind.com.cn>
2026-05-22 16:32:00 +0800

    2026.05.22代码练习

--- src/main/java/com/rsqu/leetcode/hot100/p0075_sort_colors/Hot1000075SortColors.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0075_sort_colors/Hot1000075SortColors.java
@@ -3,6 +3,23 @@ package com.rsqu.leetcode.hot100.p0075_sort_colors;
 public class Hot1000075SortColors {
 
     public void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int i = 0;
+        while (i <= right) {
+            if (nums[i] == 0) {
+                nums[i] = nums[left];
+                nums[left] = 0;
+                left++;
+                i++;
+            } else if (nums[i] == 2) {
+                nums[i] = nums[right];
+                nums[right] = 2;
+                right--;
+            } else {
+                i++;
+            }
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/rsqu/leetcode/hot100/p0078_subsets/Hot1000078Subsets.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0078_subsets/Hot1000078Subsets.java
@@ -1,11 +1,26 @@
 package com.rsqu.leetcode.hot100.p0078_subsets;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hot1000078Subsets {
 
     public List<List<Integer>> subsets(int[] nums) {
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        backtrack(nums, 0, res, temp);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start, List<List<Integer>> res, List<Integer> temp) {
+        if (start == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        temp.add(nums[start]);
+        backtrack(nums, start + 1, res, temp);
+        temp.remove(temp.size() - 1);
+        backtrack(nums, start + 1, res, temp);
     }
 
     public static void main(String[] args) {
