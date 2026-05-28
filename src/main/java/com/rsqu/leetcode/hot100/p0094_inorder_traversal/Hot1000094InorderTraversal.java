1c15750b9e8bc7249ee6e66f6335890d2ca78f21
rsqu.rose <rsqu.rose@wind.com.cn>
2026-05-28 17:18:03 +0800

    2026.05.28代码练习

--- src/main/java/com/rsqu/leetcode/hot100/p0094_inorder_traversal/Hot1000094InorderTraversal.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0094_inorder_traversal/Hot1000094InorderTraversal.java
@@ -1,12 +1,25 @@
 package com.rsqu.leetcode.hot100.p0094_inorder_traversal;
 
 import com.rsqu.leetcode.util.TreeNode;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hot1000094InorderTraversal {
 
     public List<Integer> inorderTraversal(TreeNode root) {
-        return null;
+        List<Integer> res = new ArrayList<>();
+        trace(res,root);
+        return res;
+    }
+
+    private void trace(List<Integer> res,TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        trace(res,root.left);
+        res.add(root.val);
+        trace(res,root.right);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/rsqu/leetcode/hot100/p0101_is_symmetric/Hot1000101IsSymmetric.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0101_is_symmetric/Hot1000101IsSymmetric.java
@@ -5,7 +5,23 @@ import com.rsqu.leetcode.util.TreeNode;
 public class Hot1000101IsSymmetric {
 
     public boolean isSymmetric(TreeNode root) {
-        return false;
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode leftNode, TreeNode rightNode) {
+        if (leftNode == null && rightNode == null) {
+            return true;
+        }
+        if (leftNode == null || rightNode == null) {
+            return false;
+        }
+        if (leftNode.val != rightNode.val) {
+            return false;
+        }
+        return isMirror(leftNode.left, rightNode.right) && isMirror(leftNode.right, rightNode.left);
     }
 
     public static void main(String[] args) {
