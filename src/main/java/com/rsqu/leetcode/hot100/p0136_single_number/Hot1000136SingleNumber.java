1994d783ffedd0d7fbb151827b78e77fc1246269
rsqu.rose <rsqu.rose@wind.com.cn>
2026-06-10 11:43:27 +0800

    2026.06.10代码练习

--- src/main/java/com/rsqu/leetcode/hot100/p0136_single_number/Hot1000136SingleNumber.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0136_single_number/Hot1000136SingleNumber.java
@@ -3,7 +3,11 @@ package com.rsqu.leetcode.hot100.p0136_single_number;
 public class Hot1000136SingleNumber {
 
     public int singleNumber(int[] nums) {
-        return 0;
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
