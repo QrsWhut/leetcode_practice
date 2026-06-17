a4fd1950d9e0b5494351ae52429d71be0c3e3176
rsqu.rose <rsqu.rose@wind.com.cn>
2026-06-17 13:35:41 +0800

    feat(hot100): 实现152、153和155题解法

--- src/main/java/com/rsqu/leetcode/hot100/p0153_find_min/Hot1000153FindMin.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0153_find_min/Hot1000153FindMin.java
@@ -3,7 +3,21 @@ package com.rsqu.leetcode.hot100.p0153_find_min;
 public class Hot1000153FindMin {
 
     public int findMin(int[] nums) {
-        return 0;
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[right];
     }
 
     public static void main(String[] args) {
