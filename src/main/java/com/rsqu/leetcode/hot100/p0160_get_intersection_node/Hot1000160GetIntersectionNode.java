50ab1f4214d9ba4957cef189b7bbf2dfc9e4830a
rsqu.rose <rsqu.rose@wind.com.cn>
2026-06-24 16:56:54 +0800

    feat(leetcode): 实现相交链表与多数元素算法

--- src/main/java/com/rsqu/leetcode/hot100/p0160_get_intersection_node/Hot1000160GetIntersectionNode.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0160_get_intersection_node/Hot1000160GetIntersectionNode.java
@@ -5,7 +5,43 @@ import com.rsqu.leetcode.util.ListNode;
 public class Hot1000160GetIntersectionNode {
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lengthA = getLength(headA);
+        int lengthB = getLength(headB);
+        ListNode currentA = headA;
+        ListNode currentB = headB;
+
+        if (lengthA > lengthB) {
+            currentA = moveForward(currentA, lengthA - lengthB);
+        } else {
+            currentB = moveForward(currentB, lengthB - lengthA);
+        }
+
+        while (currentA != null && currentB != null) {
+            if (currentA == currentB) {
+                return currentA;
+            }
+            currentA = currentA.next;
+            currentB = currentB.next;
+        }
         return null;
+    }
+
+    private int getLength(ListNode head) {
+        int length = 0;
+        ListNode current = head;
+        while (current != null) {
+            length++;
+            current = current.next;
+        }
+        return length;
+    }
+
+    private ListNode moveForward(ListNode head, int step) {
+        ListNode current = head;
+        for (int i = 0; i < step; i++) {
+            current = current.next;
+        }
+        return current;
     }
 
     public static void main(String[] args) {
