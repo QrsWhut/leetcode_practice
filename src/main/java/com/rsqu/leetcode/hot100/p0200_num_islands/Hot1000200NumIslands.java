1fb0891144e528f463d9c0c5e6bad96eb4d4f88a
rsqu.rose <rsqu.rose@wind.com.cn>
2026-06-26 14:43:56 +0800

    feat(leetcode): 实现岛屿数量、反转链表和课程表算法

--- src/main/java/com/rsqu/leetcode/hot100/p0200_num_islands/Hot1000200NumIslands.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0200_num_islands/Hot1000200NumIslands.java
@@ -3,7 +3,27 @@ package com.rsqu.leetcode.hot100.p0200_num_islands;
 public class Hot1000200NumIslands {
 
     public int numIslands(char[][] grid) {
-        return 0;
+        int num = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    num++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return num;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/rsqu/leetcode/hot100/p0206_reverse_list/Hot1000206ReverseList.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0206_reverse_list/Hot1000206ReverseList.java
@@ -5,8 +5,17 @@ import com.rsqu.leetcode.util.ListNode;
 public class Hot1000206ReverseList {
 
     public ListNode reverseList(ListNode head) {
-        return null;
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
     }
+
 
     public static void main(String[] args) {
         Hot1000206ReverseList solution = new Hot1000206ReverseList();
