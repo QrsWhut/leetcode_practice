b56b98eb7e453f02649ced385756ada890839dc2
rsqu.rose <rsqu.rose@wind.com.cn>
2026-07-08 11:36:55 +0800

    Generating commit message...

--- src/main/java/com/rsqu/leetcode/hot100/p0226_invert_tree/Hot1000226InvertTree.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0226_invert_tree/Hot1000226InvertTree.java
@@ -5,7 +5,19 @@ import com.rsqu.leetcode.util.TreeNode;
 public class Hot1000226InvertTree {
 
     public TreeNode invertTree(TreeNode root) {
-        return null;
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        if (root.left != null) {
+            invertTree(root.left);
+        }
+        if (root.right != null) {
+            invertTree(root.right);
+        }
+        return root;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/rsqu/leetcode/hot100/p0230_kth_smallest/Hot1000230KthSmallest.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0230_kth_smallest/Hot1000230KthSmallest.java
@@ -2,9 +2,30 @@ package com.rsqu.leetcode.hot100.p0230_kth_smallest;
 
 import com.rsqu.leetcode.util.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Hot1000230KthSmallest {
 
     public int kthSmallest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode current = root;
+
+        while (current != null || stack.isEmpty() == false) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            k--;
+            if (k == 0) {
+                return current.val;
+            }
+
+            current = current.right;
+        }
+
         return 0;
     }
 
