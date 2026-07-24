2839e49bc90774090d564a864c83ec901542d37a
rsqu.rose <rsqu.rose@wind.com.cn>
2026-07-24 13:41:10 +0800

    feat(hot100): 实现239/240/279/283/287题解法

--- src/main/java/com/rsqu/leetcode/hot100/p0279_num_squares/Hot1000279NumSquares.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0279_num_squares/Hot1000279NumSquares.java
@@ -3,7 +3,23 @@ package com.rsqu.leetcode.hot100.p0279_num_squares;
 public class Hot1000279NumSquares {
 
     public int numSquares(int n) {
-        return 0;
+        int[] memo = new int[n+1];
+        return dp(n,memo);
+    }
+
+    private int dp(int n,int[] memo) {
+        if (n == 0){
+            return 0;
+        }
+        if (memo[n] != 0){
+            return memo[n];
+        }
+        int min = Integer.MIN_VALUE;
+        for (int i = (int)Math.sqrt(n); i >= 1; i--) {
+            min = Math.min(min, 1 + dp(n - i * i, memo));
+        }
+        memo[n] = min;
+        return min;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/rsqu/leetcode/hot100/p0283_move_zeroes/Hot1000283MoveZeroes.java
+++ src/main/java/com/rsqu/leetcode/hot100/p0283_move_zeroes/Hot1000283MoveZeroes.java
@@ -1,8 +1,22 @@
 package com.rsqu.leetcode.hot100.p0283_move_zeroes;
 
+import static org.apache.commons.lang3.ArrayUtils.swap;
+
 public class Hot1000283MoveZeroes {
 
     public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                if (left != right) {
+                    swap(nums, left, right);
+                }
+                left++;
+            }
+            right++;
+        }
     }
 
     public static void main(String[] args) {
